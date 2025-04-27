b3a101980ba771edb3ec7a4c8a5d4e6146389d66
TheTechromancer <thetechromancer@protonmail.com>
2025-04-27 19:23:08 -0400

    missing files

---
+++ bbot_server/models/technology_models.py
@@ -0,0 +1,11 @@
+from pydantic import Field
+from typing import Annotated
+
+from bbot_server.utils.misc import utc_now
+from bbot_server.models.asset_models import BaseAssetFacet
+
+
+class Technology(BaseAssetFacet):
+    # unique compound index on technology+host+port makes sure we don't have duplicates
+    technology: Annotated[str, "indexed-compound:host,port", "unique", "indexed-text"]
+    last_seen: Annotated[float, "indexed"] = Field(default_factory=utc_now)
