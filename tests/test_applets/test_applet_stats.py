051118caad44c88765cc15c7ff16a35787a64cda
TheTechromancer <thetechromancer@protonmail.com>
2025-07-17 16:41:57 -0400

    improve tests

--- tests/test_applets/test_applet_stats.py
+++ tests/test_applets/test_applet_stats.py
@@ -16,140 +16,146 @@ async def test_applet_stats(bbot_server, bbot_events):
         for e in scan_events:
             await bbot_server.insert_event(e)
 
-    # wait for events to be processed
-    await asyncio.sleep(INGEST_PROCESSING_DELAY)
+    for _ in range(60):
+        # global stats
+        global_stats = await bbot_server.get_stats()
+        global_stats_ok = global_stats == {
+            "dns_links": {
+                "A": 13,
+                "AAAA": 1,
+                "CNAME": 1,
+                "TXT": 8,
+            },
+            "open_ports": {
+                "80": 3,
+                "443": 3,
+            },
+            "technologies": {
+                "cpe:/a:apache:http_server:2.4.12": 2,
+                "cpe:/a:microsoft:internet_information_services": 1,
+            },
+            "cloud_providers": {
+                "Azure": 1,
+                "Amazon": 2,
+            },
+            "findings": {
+                "max_severity": "CRITICAL",
+                "max_severity_score": 5,
+                "names": {
+                    "CVE-2024-12345": 2,
+                    "CVE-2025-54321": 2,
+                },
+                "severities": {
+                    "CRITICAL": 2,
+                    "HIGH": 2,
+                },
+                "counts_by_host": {
+                    "www.evilcorp.com": 1,
+                    "www2.evilcorp.com": 2,
+                    "api.evilcorp.com": 1,
+                },
+                "severities_by_host": {
+                    "www.evilcorp.com": {
+                        "max_severity": "HIGH",
+                        "max_severity_score": 4,
+                    },
+                    "www2.evilcorp.com": {
+                        "max_severity": "CRITICAL",
+                        "max_severity_score": 5,
+                    },
+                    "api.evilcorp.com": {
+                        "max_severity": "CRITICAL",
+                        "max_severity_score": 5,
+                    },
+                },
+            },
+        }
 
-    # global stats
-    stats = await bbot_server.get_stats()
-    assert stats == {
-        "dns_links": {
-            "A": 13,
-            "AAAA": 1,
-            "CNAME": 1,
-            "TXT": 8,
-        },
-        "open_ports": {
-            "80": 3,
-            "443": 3,
-        },
-        "technologies": {
-            "cpe:/a:apache:http_server:2.4.12": 2,
-            "cpe:/a:microsoft:internet_information_services": 1,
-        },
-        "cloud_providers": {
-            "Azure": 1,
-            "Amazon": 2,
-        },
-        "findings": {
-            "max_severity": "CRITICAL",
-            "max_severity_score": 5,
-            "names": {
-                "CVE-2024-12345": 2,
-                "CVE-2025-54321": 2,
+        # by target
+        stats_by_target = await bbot_server.get_stats(target_id=target1.id)
+        stats_by_target_ok = stats_by_target == {
+            "dns_links": {
+                "A": 9,
+                "CNAME": 1,
+                "TXT": 8,
             },
-            "severities": {
-                "CRITICAL": 2,
-                "HIGH": 2,
+            "open_ports": {
+                "80": 2,
+                "443": 3,
             },
-            "counts_by_host": {
-                "www.evilcorp.com": 1,
-                "www2.evilcorp.com": 2,
-                "api.evilcorp.com": 1,
+            "technologies": {
+                "cpe:/a:apache:http_server:2.4.12": 2,
+                "cpe:/a:microsoft:internet_information_services": 1,
             },
-            "severities_by_host": {
-                "www.evilcorp.com": {
-                    "max_severity": "HIGH",
-                    "max_severity_score": 4,
+            "cloud_providers": {
+                "Amazon": 1,
+            },
+            "findings": {
+                "max_severity": "CRITICAL",
+                "max_severity_score": 5,
+                "names": {
+                    "CVE-2024-12345": 1,
+                    "CVE-2025-54321": 2,
                 },
-                "www2.evilcorp.com": {
-                    "max_severity": "CRITICAL",
-                    "max_severity_score": 5,
+                "severities": {
+                    "CRITICAL": 2,
+                    "HIGH": 1,
                 },
-                "api.evilcorp.com": {
-                    "max_severity": "CRITICAL",
-                    "max_severity_score": 5,
+                "counts_by_host": {
+                    "api.evilcorp.com": 1,
+                    "www2.evilcorp.com": 2,
+                },
+                "severities_by_host": {
+                    "api.evilcorp.com": {
+                        "max_severity": "CRITICAL",
+                        "max_severity_score": 5,
+                    },
+                    "www2.evilcorp.com": {
+                        "max_severity": "CRITICAL",
+                        "max_severity_score": 5,
+                    },
                 },
             },
-        },
-    }
+        }
 
-    # by target
-    stats = await bbot_server.get_stats(target_id=target1.id)
-    assert stats == {
-        "dns_links": {
-            "A": 9,
-            "CNAME": 1,
-            "TXT": 8,
-        },
-        "open_ports": {
-            "80": 2,
-            "443": 3,
-        },
-        "technologies": {
-            "cpe:/a:apache:http_server:2.4.12": 2,
-            "cpe:/a:microsoft:internet_information_services": 1,
-        },
-        "cloud_providers": {
-            "Amazon": 1,
-        },
-        "findings": {
-            "max_severity": "CRITICAL",
-            "max_severity_score": 5,
-            "names": {
-                "CVE-2024-12345": 1,
-                "CVE-2025-54321": 2,
+        # by domain
+        stats_by_domain = await bbot_server.get_stats(domain="www2.evilcorp.com")
+        stats_by_domain_ok = stats_by_domain == {
+            "dns_links": {
+                "A": 2,
             },
-            "severities": {
-                "CRITICAL": 2,
-                "HIGH": 1,
+            "open_ports": {
+                "80": 1,
             },
-            "counts_by_host": {
-                "api.evilcorp.com": 1,
-                "www2.evilcorp.com": 2,
-            },
-            "severities_by_host": {
-                "api.evilcorp.com": {
-                    "max_severity": "CRITICAL",
-                    "max_severity_score": 5,
+            "technologies": {},
+            "cloud_providers": {},
+            "findings": {
+                "max_severity": "CRITICAL",
+                "max_severity_score": 5,
+                "names": {
+                    "CVE-2024-12345": 1,
+                    "CVE-2025-54321": 1,
                 },
-                "www2.evilcorp.com": {
-                    "max_severity": "CRITICAL",
-                    "max_severity_score": 5,
+                "severities": {
+                    "CRITICAL": 1,
+                    "HIGH": 1,
+                },
+                "counts_by_host": {
+                    "www2.evilcorp.com": 2,
+                },
+                "severities_by_host": {
+                    "www2.evilcorp.com": {
+                        "max_severity": "CRITICAL",
+                        "max_severity_score": 5,
+                    },
                 },
             },
-        },
-    }
+        }
 
-    # by domain
-    stats = await bbot_server.get_stats(domain="www2.evilcorp.com")
-    assert stats == {
-        "dns_links": {
-            "A": 2,
-        },
-        "open_ports": {
-            "80": 1,
-        },
-        "technologies": {},
-        "cloud_providers": {},
-        "findings": {
-            "max_severity": "CRITICAL",
-            "max_severity_score": 5,
-            "names": {
-                "CVE-2024-12345": 1,
-                "CVE-2025-54321": 1,
-            },
-            "severities": {
-                "CRITICAL": 1,
-                "HIGH": 1,
-            },
-            "counts_by_host": {
-                "www2.evilcorp.com": 2,
-            },
-            "severities_by_host": {
-                "www2.evilcorp.com": {
-                    "max_severity": "CRITICAL",
-                    "max_severity_score": 5,
-                },
-            },
-        },
-    }
+        if global_stats_ok and stats_by_target_ok and stats_by_domain_ok:
+            break
+
+        await asyncio.sleep(0.5)
+
+    else:
+        assert False, f"Stats are not ok. global_stats_ok: {global_stats_ok}, stats_by_target_ok: {stats_by_target_ok}, stats_by_domain_ok: {stats_by_domain_ok}"
