7e2025ac5c2d231318dc8a4ac31cf8b667072f97
TheTechromancer <thetechromancer@protonmail.com>
2025-07-30 11:34:13 -0400

    technology summarization, lots of tests

--- bbot_server/modules/technologies/technologies_cli.py
+++ bbot_server/modules/technologies/technologies_cli.py
@@ -24,12 +24,53 @@ class TechnologyCTL(BaseBBCTL):
         target_id: Annotated[
             str, typer.Option("--target", "-t", help="filter by target (can be either name or ID)")
         ] = None,
+        sort: Annotated[str, typer.Option("--sort", help="field to sort by")] = ["-last_seen"],
     ):
+        technologies = self.bbot_server.get_technologies(
+            domain=domain, host=host, technology=technology, target_id=target_id, search=search, sort=sort
+        )
+
         if json:
-            for technology in self.bbot_server.get_technologies(
-                domain=domain, host=host, technology=technology, target_id=target_id, search=search
-            ):
+            for technology in technologies:
                 self.print_pydantic_json(technology)
+            return
+
+        table = self.Table()
+        table.add_column("Technology", style=self.COLOR)
+        table.add_column("Host and Port", style="bold")
+        table.add_column("Last Seen", style=self.DARK_COLOR)
+        for t in technologies:
+            table.add_row(
+                t.technology,
+                t.netloc,
+                self.timestamp_to_human(t.last_seen),
+            )
+        self.stdout.print(table)
+
+    @subcommand(help="Summarize technologies")
+    def summarize(
+        self,
+        json: common.json = False,
+        domain: Annotated[str, typer.Option("--domain", "-d", help="filter by domain (subdomains included)")] = None,
+        host: Annotated[str, typer.Option("--host", "-h", help="filter by host")] = None,
+        technology: Annotated[
+            str, typer.Option("--technology", "-t", help="filter by technology (must match exactly)")
+        ] = None,
+        target_id: Annotated[str, typer.Option("--target", help="filter by target (can be either name or ID)")] = None,
+        limit: Annotated[
+            int,
+            typer.Option("--limit", "-l", help="limit the number of results (most prevalent results are shown first)"),
+        ] = None,
+    ):
+        if limit is not None and limit < 1:
+            raise self.BBOTServerValueError("Limit must be greater than 0")
+
+        summary = self.bbot_server.get_technologies_summary(
+            domain=domain, host=host, technology=technology, target_id=target_id
+        )
+        if json:
+            for technology in summary[:limit]:
+                self.print_json(technology)
             return
 
         table = self.Table()
@@ -37,9 +78,7 @@ class TechnologyCTL(BaseBBCTL):
         table.add_column("Number of Hosts")
         table.add_column("Hosts", style="bold")
         table.add_column("Last Seen", style=self.DARK_COLOR)
-        for t in self.bbot_server.get_technologies_summary(
-            domain=domain, host=host, technology=technology, target_id=target_id
-        ):
+        for t in summary[:limit]:
             table.add_row(
                 t["technology"],
                 f"{len(t['hosts']):,}",
--- tests/test_cli/test_cli_technologyctl.py
+++ tests/test_cli/test_cli_technologyctl.py
@@ -2,8 +2,10 @@ import orjson
 import subprocess
 from time import sleep
 
+from bbot_server.modules.targets.targets_models import Target
+
 from bbot_server.modules.technologies.technology_models import Technology
-from tests.conftest import BBCTL_COMMAND, INGEST_PROCESSING_DELAY
+from tests.conftest import BBCTL_COMMAND, INGEST_PROCESSING_DELAY, BBOT_SERVER_TEST_DIR
 
 
 def test_cli_technologyctl(bbot_server_http, bbot_watchdog, bbot_out_file):
@@ -103,12 +105,13 @@ def test_cli_technologyctl(bbot_server_http, bbot_watchdog, bbot_out_file):
     command = BBCTL_COMMAND + ["technology", "list"]
     process = subprocess.run(command, capture_output=True, text=True)
     assert process.returncode == 0
-    assert process.stdout.count("cpe:/a:apache") == 1
+    assert process.stdout.count("cpe:/a:apache") == 3
     assert process.stdout.count("cpe:/a:microsoft") == 1
     assert "t1.tech.evil" in process.stdout
+    assert "t2.tech.evil" in process.stdout
 
     # search technologies (JSON)
-    command = BBCTL_COMMAND + ["technology", "search", "apache", "--json"]
+    command = BBCTL_COMMAND + ["technology", "list", "--search", "apache", "--json"]
     process = subprocess.run(command, capture_output=True, text=True)
     assert process.returncode == 0
     assert len(process.stdout.splitlines()) == 3
@@ -121,14 +124,14 @@ def test_cli_technologyctl(bbot_server_http, bbot_watchdog, bbot_out_file):
     }
 
     # search technologies (text)
-    command = BBCTL_COMMAND + ["technology", "search", "apache"]
+    command = BBCTL_COMMAND + ["technology", "list", "--search", "apache"]
     process = subprocess.run(command, capture_output=True, text=True)
     assert process.returncode == 0
     # should only match apache and not IIS
     assert process.stdout.count("cpe:/a:apache") == 3
     assert not "internet" in process.stdout
 
-    command = BBCTL_COMMAND + ["technology", "search", "microsoft"]
+    command = BBCTL_COMMAND + ["technology", "list", "--search", "microsoft"]
     process = subprocess.run(command, capture_output=True, text=True)
     assert process.returncode == 0
     assert process.stdout.count("cpe:/a:microsoft") == 1
@@ -171,21 +174,73 @@ def test_cli_technologyctl(bbot_server_http, bbot_watchdog, bbot_out_file):
     assert process.returncode == 0
     assert process.stdout == ""
 
-    # # TODO: filter technologies by target id
-    # target_file = BBOT_SERVER_TEST_DIR / "targets"
-    # target_file.write_text("t2.tech.evilcorp.com")
-    # command = BBCTL_COMMAND + ["scan", "target", "create", "--seeds", target_file, "--name", "evilcorp1"]
-    # process = subprocess.run(command, capture_output=True, text=True)
-    # assert process.returncode == 0
+    # filter technologies by target id
+    # create a new target that matches two technologies
+    target_file = BBOT_SERVER_TEST_DIR / "targets"
+    target_file.write_text("t2.tech.evilcorp.com")
+    command = BBCTL_COMMAND + ["scan", "target", "create", "--seeds", target_file, "--name", "evilcorp1"]
+    process = subprocess.run(command, capture_output=True, text=True)
+    assert process.returncode == 0
 
-    # # wait for a sec for the target to be processed
-    # sleep(1)
-    # command = BBCTL_COMMAND + ["technology", "list", "--target", "evilcorp1", "--json"]
-    # process = subprocess.run(command, capture_output=True, text=True)
-    # assert process.returncode == 0
-    # technologies = [Technology(**orjson.loads(line)) for line in process.stdout.splitlines()]
-    # assert len(technologies) == 2
-    # assert {(t.netloc, t.technology) for t in technologies} == {
-    #     ("t2.tech.evilcorp.com:443", "cpe:/a:apache:http_server:2.4.12"),
-    #     ("t2.tech.evilcorp.com:443", "cpe:/a:microsoft:internet_information_services"),
-    # }
+    # wait for the target to be processed
+    for _ in range(60):
+        command = BBCTL_COMMAND + ["scan", "target", "list", "--json"]
+        process = subprocess.run(command, capture_output=True, text=True)
+        assert process.returncode == 0
+        targets = [Target(**orjson.loads(line)) for line in process.stdout.splitlines()]
+        if len(targets) == 1:
+            break
+        sleep(0.5)
+    else:
+        assert False, "Target not created successfully"
+
+    # list technologies for the target (JSON)
+    for _ in range(60):
+        command = BBCTL_COMMAND + ["technology", "list", "--target", "evilcorp1", "--json"]
+        process = subprocess.run(command, capture_output=True, text=True)
+        assert process.returncode == 0
+        technologies = [Technology(**orjson.loads(line)) for line in process.stdout.splitlines()]
+        if len(technologies) == 2 and {(t.netloc, t.technology) for t in technologies} == {
+            ("t2.tech.evilcorp.com:443", "cpe:/a:apache:http_server:2.4.12"),
+            ("t2.tech.evilcorp.com:443", "cpe:/a:microsoft:internet_information_services"),
+        }:
+            break
+        sleep(0.5)
+    else:
+        assert False, "Technologies not found for target"
+
+    # list technologies for the target (text)
+    for _ in range(60):
+        command = BBCTL_COMMAND + ["technology", "list", "--target", "evilcorp1"]
+        process = subprocess.run(command, capture_output=True, text=True)
+        if (
+            process.returncode == 0
+            and process.stdout.count("cpe:/a:apache") == 1
+            and process.stdout.count("cpe:/a:microsoft") == 1
+        ):
+            break
+        sleep(0.5)
+    else:
+        assert False, "Technologies not found for target"
+
+    # summarize technologies (JSON)
+    command = BBCTL_COMMAND + ["technology", "summarize", "--json"]
+    process = subprocess.run(command, capture_output=True, text=True)
+    assert process.returncode == 0
+    summary = [orjson.loads(line) for line in process.stdout.splitlines()]
+    assert len(summary) == 2
+    apache_summary = [t for t in summary if t["technology"] == "cpe:/a:apache:http_server:2.4.12"]
+    assert len(apache_summary) == 1
+    assert apache_summary[0]["hosts"] == ["t1.tech.evilcorp.com", "t2.tech.evilcorp.com"]
+    microsoft_summary = [t for t in summary if t["technology"] == "cpe:/a:microsoft:internet_information_services"]
+    assert len(microsoft_summary) == 1
+    assert microsoft_summary[0]["hosts"] == ["t2.tech.evilcorp.com"]
+
+    # summarize technologies (text)
+    command = BBCTL_COMMAND + ["technology", "summarize"]
+    process = subprocess.run(command, capture_output=True, text=True)
+    assert process.returncode == 0
+    assert process.stdout.count("cpe:/a:apache") == 1
+    assert process.stdout.count("cpe:/a:microsoft") == 1
+    assert "t1.tech.evil" in process.stdout
+    assert "t2.tech.evil" in process.stdout
